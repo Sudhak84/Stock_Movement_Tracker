7b14bf7fee3821646601a41ed932db7af867e32c
Sudhak84 <sudhak84@gmail.com>
2021-07-31 21:23:46 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2,3 +2,45 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from nsepy import get_history
+from datetime import datetime
+
+# Getting the dates from the user
+start=datetime(2015,1,1)
+end=datetime(2020,10,23)
+
+st.set_page_config(layout="wide")
+
+#Getting the stock details from the users
+stock_name=['PNB']
+percent_change=5.0
+data=get_history(symbol=stock_name[0],start=start,end=end)
+
+
+Company_list=['HDFC','RELIANCE','SBIN','LT']
+len(Company_list)
+
+tmp_change=[]
+tmp_category=[]
+tmp=data['Prev Close'].iloc[0]
+#print(tmp)
+for index,rows in data.iterrows():
+    ls_change=((rows['Close']-tmp)/tmp)
+    #print(((rows['Close']-tmp)/tmp)*100)
+    if ((rows['Close']-tmp)/tmp)*100>percent_change:
+        ls_category="Positive"
+    elif ((rows['Close']-tmp)/tmp)*100<-percent_change:
+        ls_category="Negative"
+    else:
+        ls_category="Neutral"
+    #print((rows['Close']-tmp)/tmp)
+    if abs((rows['Close']-tmp)/tmp)*100>percent_change:
+        #print(abs((rows['Close']-tmp)/tmp)*100)
+        tmp=rows['Close']
+        #print(tmp)
+    #rows['tmp_check']=
+    tmp_change.append(ls_change)
+    tmp_category.append(ls_category)
+data['Change%']=tmp_change
+data['Category']=tmp_category
+#data.to_csv(str(stock_name[0])+'.csv')
