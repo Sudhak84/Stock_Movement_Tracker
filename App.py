1936b19037194d89433a1018e3c98e58176817b5
Sudhak84 <sudhak84@gmail.com>
2021-07-31 22:00:19 +0530

    Update App.py

--- App.py
+++ App.py
@@ -6,46 +6,46 @@ from nsepy import get_history
 from datetime import datetime
 
 # Getting the dates from the user
-start=datetime(2015,1,1)
-end=datetime(2020,10,23)
+start=datetime(2021,1,1)
+end=datetime(2021,7,30)
 
 st.title("Stock Movement Tracker")
 
-st.text_input("Enter the ticker here","RIL")
+user_input = st.text_input("Enter the ticker here","RIL")
 
 #st.set_page_config(layout="wide")
 
 #Getting the stock details from the users
-stock_name=['PNB']
+stock_name=[user_input]
 percent_change=5.0
 data=get_history(symbol=stock_name[0],start=start,end=end)
+st.write(data.shape)
 
+#Company_list=['HDFC','RELIANCE','SBIN','LT']
+#len(Company_list)
 
-Company_list=['HDFC','RELIANCE','SBIN','LT']
-len(Company_list)
-
-tmp_change=[]
-tmp_category=[]
-tmp=data['Prev Close'].iloc[0]
-#print(tmp)
-for index,rows in data.iterrows():
-    ls_change=((rows['Close']-tmp)/tmp)
-    #print(((rows['Close']-tmp)/tmp)*100)
-    if ((rows['Close']-tmp)/tmp)*100>percent_change:
-        ls_category="Positive"
-    elif ((rows['Close']-tmp)/tmp)*100<-percent_change:
-        ls_category="Negative"
-    else:
-        ls_category="Neutral"
-    #print((rows['Close']-tmp)/tmp)
-    if abs((rows['Close']-tmp)/tmp)*100>percent_change:
-        #print(abs((rows['Close']-tmp)/tmp)*100)
-        tmp=rows['Close']
-        #print(tmp)
-    #rows['tmp_check']=
-    tmp_change.append(ls_change)
-    tmp_category.append(ls_category)
-data['Change%']=tmp_change
-data['Category']=tmp_category
+#tmp_change=[]
+#tmp_category=[]
+#tmp=data['Prev Close'].iloc[0]
+##print(tmp)
+#for index,rows in data.iterrows():
+#    ls_change=((rows['Close']-tmp)/tmp)
+#    #print(((rows['Close']-tmp)/tmp)*100)
+#    if ((rows['Close']-tmp)/tmp)*100>percent_change:
+#        ls_category="Positive"
+#    elif ((rows['Close']-tmp)/tmp)*100<-percent_change:
+#        ls_category="Negative"
+ #   else:
+ #       ls_category="Neutral"
+ #   #print((rows['Close']-tmp)/tmp)
+ #   if abs((rows['Close']-tmp)/tmp)*100>percent_change:
+ #       #print(abs((rows['Close']-tmp)/tmp)*100)
+ #       tmp=rows['Close']
+ #       #print(tmp)
+ #   #rows['tmp_check']=
+ #   tmp_change.append(ls_change)
+ #   tmp_category.append(ls_category)
+#data['Change%']=tmp_change
+#data['Category']=tmp_category
 #data.to_csv(str(stock_name[0])+'.csv')
-streamlit.dataframe(data=data, width=None, height=None)
+#streamlit.dataframe(data=data, width=None, height=None)
